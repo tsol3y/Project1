11fbce1fd7ee2cd61d7c29ed662dac6d0e6eb016
rachaelrogan <47510726+rachaelrogan@users.noreply.github.com>
2020-02-14 14:37:07 -0500

    end of class 2/14
    worked on updateBitmap method

--- Proj1/Client.cs
+++ Proj1/Client.cs
@@ -8,14 +8,38 @@ using System.Buffers.Binary;
 namespace Client {
     class Program {
         static void Main(string[] args) {
-
-            var testInt = 1024;
-            byte[] returnBytes = new byte[11];
-            var byteSpan = new Span<byte>(returnBytes);
-            BinaryPrimitives.WriteInt32BigEndian(byteSpan.Slice(3,4), testInt);
+            RayTracer.Color[,] bitmap = new Color[400, 400];
+            RayTracer.RayTracer rayTracer = new RayTracer.RayTracer(400, 400,
+                (int x, int y, Color color) => { bitmap[x, y] = color; });;
             
-            var testResult = BinaryPrimitives.ReadInt32BigEndian(byteSpan.Slice(3, 4));
-            Console.WriteLine(testResult);
+            byte[] returnBytes = new byte[11];
+            returnBytes[0] = RayTracer.RayTracerApp.ToByte(200.0);
+            returnBytes[1] = RayTracer.RayTracerApp.ToByte(180.0);
+            returnBytes[2] = RayTracer.RayTracerApp.ToByte(160.0);
+            var byteSpan = new Span<byte>(returnBytes);
+            BinaryPrimitives.WriteInt32BigEndian(byteSpan.Slice(3,4), 300);
+            BinaryPrimitives.WriteInt32BigEndian(byteSpan.Slice(7,4), 250);
+            updateBitMap(returnBytes, bitmap);
+            Console.WriteLine(bitmap[300,250].R);
+            Console.WriteLine(bitmap[300,250].G);
+            Console.WriteLine(bitmap[300,250].B);
+
+
+
+
+
+
+            // var testResult = BinaryPrimitives.ReadInt32BigEndian(byteSpan.Slice(3, 4));
+            // Console.WriteLine(testResult);
+            // double testDouble = 156;
+            // var testByte = RayTracerApp.ToByte(testDouble);
+            // var testByte = Convert.ToByte(testDouble);
+            // byte[] testByteArray = new byte[8];
+            // testByteArray[7] = testByte;
+            // var returnDouble = Convert.ToDouble(testByte);
+            // if we wanted to, we could put the testByte into a byte[] and then convert it to a Double
+            // Console.WriteLine(returnDouble);
+
 
             /*var utf8 = new UTF8Encoding();
             UdpClient c = new UdpClient(3333);
@@ -29,6 +53,18 @@ namespace Client {
             
             RayTracer.RayTracerApp.WritePPM("test", bitmap);*/
         }
+
+        static void updateBitMap(byte[] update, Color[,] bitmapToEdit) {
+            var byteSpan = new Span<byte>(update);
+            var R = Convert.ToDouble(update[0]);
+            var G = Convert.ToDouble(update[1]);
+            var B = Convert.ToDouble(update[2]);
+            var X = BinaryPrimitives.ReadInt32BigEndian(byteSpan.Slice(3, 4));
+            var Y = BinaryPrimitives.ReadInt32BigEndian(byteSpan.Slice(7, 4));
+            bitmapToEdit[X,Y] = new Color(R, G, B);
+            // This doesn't do anything yet. We need to convert R G B bytes into doubles to make a Color object
+        }
+
     }
 }
 
